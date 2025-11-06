a0c871211f936faf37792452005bdd0a9b14b5e9
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-11-06 21:35:45 +0800

    chore: update dependencies, docker compose, migrate script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,9 @@
-migrate -verbose -path=./db/migrations -database postgres://admin:adminsecret@localhost:5432/social?sslmode=disable up
+case "$1" in
+  up|down)
+    migrate -verbose -path=./db/migrations -database postgres://admin:adminsecret@localhost:5432/social?sslmode=disable up
+    ;;
+  *)
+    echo "you have to pass 'up' or 'down' as an argument"
+    exit 1
+    ;;
+esac
